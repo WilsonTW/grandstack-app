4c9abeefeb4248b2899c1603b0f1b50b9e3b81c4
wilson <sogoph87@gmail.com>
2022-09-08 17:59:57 +0800

    add Wilson

--- web-react/src/App.js
+++ web-react/src/App.js
@@ -176,7 +176,7 @@ export default function App() {
               noWrap
               className={classes.title}
             >
-              Welcome To GRANDstack App
+              Welcome To Wilson's GRANDstack App 
             </Typography>
           </Toolbar>
         </AppBar>
