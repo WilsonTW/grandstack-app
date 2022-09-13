1a4d81ad4dd21d7a6b549d70842b0a8bdbef7ba6
wilson <sogoph87@gmail.com>
2022-09-13 15:44:05 +0800

    change to Aura DB

--- web-react/src/App.js
+++ web-react/src/App.js
@@ -176,7 +176,7 @@ export default function App() {
               noWrap
               className={classes.title}
             >
-              Wilson GRANDstack App
+              Wilson GRANDstack App - Based on Aura DB
             </Typography>
           </Toolbar>
         </AppBar>
